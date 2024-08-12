decb428e786b8d2849c189838ccceb6ef02eb955
DucThien1309 <thiennguyen130922@gmail.com>
2024-08-12 16:02:42 +0700

    Fix https

--- Duan_Laravel/vite.config.js
+++ Duan_Laravel/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  server: { https: true},
+  server: { https: false},
   plugins: [
         laravel([
             'resources/css/app.css', 
